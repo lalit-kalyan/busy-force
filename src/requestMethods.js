79b708f88269c34adf93fb35069cc87e50e0e775
lalit <lalit.rishikalyan@gmail.com>
2023-03-14 11:04:15 +0530

    little

--- src/requestMethods.js
+++ src/requestMethods.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 const BASE_URL = "https://ill-pear-dove-sari.cyclic.app/api";
+//const BASE_URL = "http://localhost:5000/api";
 
 export const IMAGE_URL =
   "https://api.cloudinary.com/v1_1/kalyanmitthu/image/upload";
